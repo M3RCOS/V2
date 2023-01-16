bdf6d3d60e11a151ae7ffd5bda7f8444e12496cc
Raad MARCOS <80981959+M3RCOS@users.noreply.github.com>
2023-01-16 17:06:25 +0300

    Add files via upload

--- MARCOS.lua
+++ MARCOS.lua
@@ -91,7 +91,7 @@ Token = Information.Token
 UserBot = Information.UserBot
 MARCOS = Token:match("(%d+)")
 os.execute('sudo rm -fr .CallBack-Bot/'..MARCOS)
-bot = Fx.MARCOSbots.set_config{api_id=29636630,api_hash='7113f88c9568f747ac49c8ed72ed6952',session_name=MARCOS,token=Token}
+bot = Fx.set_config{api_id=29636630,api_hash='7113f88c9568f747ac49c8ed72ed6952',session_name=MARCOS,token=Token}
 function coin(coin)
 local Coins = tostring(coin)
 local Coins = Coins:gsub('٠','0')
