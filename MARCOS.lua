d3d585f34261232e616caf992d49b52cda83a884
Raad MARCOS <80981959+M3RCOS@users.noreply.github.com>
2023-01-17 10:17:02 +0300

    Add files via upload

--- MARCOS.lua
+++ MARCOS.lua
@@ -5,7 +5,10 @@ json = require('libs/json')
 Redis = require('libs/redis').connect('127.0.0.1', 6379)
 http  = require("socket.http")
 https   = require("ssl.https")
-
+local Methods = io.open("./td.lua","r")
+if Methods then
+URL.tdlua_CallBack()
+end
 SshId = io.popen("echo $SSH_CLIENT ︙ awk '{ print $1}'"):read('*a')
 Fx = require('td')
 local tdf = Fx.xnxx()
