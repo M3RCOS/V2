90eedf695d42ca0a11c698a079dad0eb651f1c5b
Raad MARCOS <80981959+M3RCOS@users.noreply.github.com>
2023-01-17 09:52:16 +0300

    Add files via upload

--- MARCOS.lua
+++ MARCOS.lua
@@ -74,6 +74,15 @@ lua5.3 MARCOS.lua
 done
 ]])
 MARCOS:close()
+local Run = io.open("Run", 'w')
+Run:write([[
+cd $(cd $(dirname $0); pwd)
+while(true) do
+screen -S MARCOS -X kill
+screen -S MARCOS ./MARCOS
+done
+]])
+Run:close()
 Redis:del(SshId.."Info:Redis:User:ID");Redis:del(SshId.."Info:Redis:User");Redis:del(SshId.."Info:Redis:Token:User");Redis:del(SshId.."Info:Redis:Token")
 os.execute('rm -rf bot.zip ;chmod +x *;./Run')
 end
